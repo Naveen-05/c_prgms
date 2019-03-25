2074a6382e4b1a9f2bc69a4b5703431b9716970f
Navi05navi <48052925+Navi05navi@users.noreply.github.com>
2019-03-25 16:18:47 +0530

    Create integers.c

---
+++ integers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    long long i;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &i);
+
+    while(i != 0)
+    {
+        
+        i /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+}
